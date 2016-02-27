fe474105d5d1b6beef375aad7dbff33b9afe7766
hasan <kodpark@hotmail.com>
2016-02-27 02:29:50 +0200

    ok tamam

--- Shredder/src/frmMain.java
+++ Shredder/src/frmMain.java
@@ -13,11 +13,15 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.Box.Filler;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
@@ -25,106 +29,176 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingUtilities;
 import javax.swing.JList;
 import java.awt.Component;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
+import javax.swing.JProgressBar;
 
 public class frmMain extends JFrame {
 	private JTextField txtPath;
 	private DefaultListModel listModel;
+	List<String> results;
+
 	public frmMain() {
+		setTitle("File Shredder");
 
-		 this.setSize(822,700);
-		 
-		 this.setMaximumSize(getSize());
-		 getContentPane().setLayout(new BorderLayout(0, 0));
-		 
-		 JMenuBar menuBar = new JMenuBar();
-		 getContentPane().add(menuBar, BorderLayout.NORTH);
-		 
-		 JMenu fileMenu = new JMenu("Dosya");
-		 menuBar.add(fileMenu);
-		 
-		 JMenuItem mnitmExit = new JMenuItem("Exit");
-		 mnitmExit.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent arg0) {
-		 		System.exit(0);
-		 	}
-		 });
-		
-		 fileMenu.add(mnitmExit);
-		 
-		 JMenu menuEdit = new JMenu("Wipe Metodu");
-		 menuBar.add(menuEdit);
-		 
-		 JRadioButtonMenuItem rdbtnmnitmZeroPass = new JRadioButtonMenuItem("Zero Pass");
-		 menuEdit.add(rdbtnmnitmZeroPass);
-		 
-		 JRadioButtonMenuItem rdbtnmnitmDodPass = new JRadioButtonMenuItem("DoD 3 Pass");
-		 menuEdit.add(rdbtnmnitmDodPass);
-		 ButtonGroup bg=new ButtonGroup();
-		 bg.add(rdbtnmnitmDodPass);
-		 bg.add(rdbtnmnitmZeroPass);
-		 
-		 JPanel panel = new JPanel();
-		 getContentPane().add(panel, BorderLayout.CENTER);
-		 panel.setLayout(new BorderLayout(0, 0));
-		 
-		 JPanel panel_1 = new JPanel();
-		 panel.add(panel_1, BorderLayout.NORTH);
-		 
-		 JLabel lblNewLabel = new JLabel("Silinecek Dizin:");
-		 
-		 txtPath = new JTextField();
-		 txtPath.setColumns(10);
-		 
-		 JButton btnSelectPath = new JButton("...");
-		 btnSelectPath.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent arg0) {
-		 		JFileChooser jfile=new JFileChooser();
-		 		int i=jfile.showOpenDialog(this);
-		 	}
-		 });
-		 GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		 gl_panel_1.setHorizontalGroup(
-		 	gl_panel_1.createParallelGroup(Alignment.LEADING)
-		 		.addGroup(gl_panel_1.createSequentialGroup()
-		 			.addGap(5)
-		 			.addComponent(lblNewLabel)
-		 			.addGap(5)
-		 			.addComponent(txtPath, GroupLayout.PREFERRED_SIZE, 637, GroupLayout.PREFERRED_SIZE)
-		 			.addPreferredGap(ComponentPlacement.UNRELATED)
-		 			.addComponent(btnSelectPath, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
-		 			.addGap(31))
-		 );
-		 gl_panel_1.setVerticalGroup(
-		 	gl_panel_1.createParallelGroup(Alignment.LEADING)
-		 		.addGroup(gl_panel_1.createSequentialGroup()
-		 			.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-		 				.addGroup(gl_panel_1.createSequentialGroup()
-		 					.addGap(8)
-		 					.addComponent(lblNewLabel))
-		 				.addGroup(gl_panel_1.createSequentialGroup()
-		 					.addGap(5)
-		 					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
-		 						.addComponent(txtPath, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		 						.addComponent(btnSelectPath))))
-		 			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		 );
-		 panel_1.setLayout(gl_panel_1);
-		 
-		 JPanel panel_2 = new JPanel();
-		 panel.add(panel_2, BorderLayout.CENTER);
-		 panel_2.setLayout(new BorderLayout(0, 0));
-		 
-		 JList list = new JList();
-		 panel_2.add(list);
-		 list.setModel(ListItem.GetListModel());
-		 
-	}
-@Override
-protected void finalize() throws Throwable {
-	// TODO Auto-generated method stub
-	super.finalize();
+		this.setSize(822, 700);
+
+		this.setMaximumSize(new Dimension(800, 700));
+		getContentPane().setLayout(new BorderLayout(0, 0));
+
+		JMenuBar menuBar = new JMenuBar();
+		getContentPane().add(menuBar, BorderLayout.NORTH);
+
+		JMenu fileMenu = new JMenu("Dosya");
+		menuBar.add(fileMenu);
+
+		JMenuItem mnitmExit = new JMenuItem("Exit");
+		mnitmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+
+		fileMenu.add(mnitmExit);
+
+		JMenu menuEdit = new JMenu("Wipe Metodu");
+		menuBar.add(menuEdit);
+
+		JRadioButtonMenuItem rdbtnmnitmZeroPass = new JRadioButtonMenuItem("Zero Pass");
+		menuEdit.add(rdbtnmnitmZeroPass);
+
+		JRadioButtonMenuItem rdbtnmnitmDodPass = new JRadioButtonMenuItem("DoD 3 Pass");
+		menuEdit.add(rdbtnmnitmDodPass);
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rdbtnmnitmDodPass);
+		bg.add(rdbtnmnitmZeroPass);
+
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+
+		JLabel lblNewLabel = new JLabel("Silinecek Dizin:");
+
+		txtPath = new JTextField();
+		txtPath.setDisabledTextColor(Color.WHITE);
+		txtPath.setEditable(false);
+		txtPath.setColumns(10);
+
+		JButton btnSelectPath = new JButton("...");
+		btnSelectPath.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser jfile = new JFileChooser();
+				jfile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int i = jfile.showOpenDialog(frmMain.this);
+				if (i == JFileChooser.APPROVE_OPTION) {
+					txtPath.setText(jfile.getSelectedFile().getAbsolutePath());
+					results = new ArrayList<String>();
+					ListItem.GetListModel().clear();
+					LoadListByPath(txtPath.getText());
+					frmMain.this.setTitle(String.valueOf(results.size()));
+				}
+			}
+		});
+
+		JButton btnStart = new JButton("");
+
+		btnStart.setIcon(new ImageIcon(
+				frmMain.class.getResource("/com/sun/javafx/webkit/prism/resources/mediaPlayDisabled.png")));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1
+				.setHorizontalGroup(
+						gl_panel_1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_1.createSequentialGroup().addGap(5).addComponent(lblNewLabel)
+										.addGap(5)
+										.addComponent(txtPath, GroupLayout.PREFERRED_SIZE, 637,
+												GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addComponent(btnSelectPath, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addComponent(btnStart, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE).addGap(54)));
+		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+						.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_1.createSequentialGroup().addGap(8).addComponent(lblNewLabel))
+								.addGroup(gl_panel_1.createSequentialGroup().addGap(5)
+										.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtPath, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(btnSelectPath).addComponent(btnStart))))
+				.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		panel_1.setLayout(gl_panel_1);
+		JList list = new JList();
+		list.setModel(ListItem.GetListModel());
+		JScrollPane scrollPane = new JScrollPane(list);
+		panel.add(scrollPane, BorderLayout.CENTER);
+
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		getContentPane().add(progressBar, BorderLayout.SOUTH);
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				progressBar.setMaximum(ListItem.GetListModel().getSize());
+				Thread t = new Thread() {
+					public void run() {
+
+						for (int i = 0; i <= ListItem.GetListModel().getSize(); i++) {
+							final int t = i;
+							SwingUtilities.invokeLater(new Runnable() {
+								public void run() {
+									
+									progressBar.setValue(t);
+									int tmp=(t*100)/ListItem.GetListModel().getSize();
+									progressBar.setString(tmp+" % ");
+									
+								}
+							});
+try {
+	Thread.sleep(0, 100);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
 }
+						}
+					}
+				};
+				t.start();
+			}
+		});
 
+	}
+
+	protected void LoadListByPath(String text) {
+
+		File f = new File(text);
+		if (f.exists() && f.isDirectory()) {
+			results.add(text);
+
+			File[] list = f.listFiles();
+			if (list != null) {
+				for (int i = 0; i < list.length; i++) {
+					if (list[i].isFile())
+						ListItem.GetListModel().AddListItem(list[i].getAbsolutePath());
+					else if (list[i].isDirectory()) {
+
+						LoadListByPath(list[i].getAbsolutePath());
+					}
+				}
+			} else {
+				System.err.println(text);
+			}
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
 }
